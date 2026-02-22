843fd56e6abe21b3653c030d81caa4e8a7883910
Danae Salazar <danae.salazar357@gmail.com>
2026-02-22 14:37:48 -0600

    added ldh logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 import LoginForm from "@/components/login-form";
 
 export default async function LoginPage() {
@@ -10,8 +11,11 @@ export default async function LoginPage() {
   }
 
   return (
-    <div className="min-h-screen bg-background flex items-center justify-center px-4">
-      <div className="w-full max-w-sm">
+    <div className="w-full min-h-screen bg-background flex-col items-center center justify-center px-4">
+      <div className="flex items-center justify-center p-4">
+        <Image src={"./logo-negro.svg"} alt={"Escudo LDH"} width={300} height={"300"}/>
+      </div>
+      <div className="flex items-center justify-center p-4">
         <div className="bg-card rounded-2xl border border-border p-8 shadow-sm">
           <div className="flex flex-col items-center gap-2 mb-8">
             <h1 className="text-2xl font-bold text-card-foreground font-sans tracking-tight">
